fee635f1b31db240d6fa63109ddc4a380f19bcb2
ShevchenkoVladyslav <48383830+ShevchenkoVladyslav@users.noreply.github.com>
2021-10-28 16:35:24 +0300

    Completed HW

---
+++ ModuleHW/ModuleHW/Publisher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ModuleHW
+{
+    public class Publisher
+    {
+        private readonly int _id;
+        private readonly ThreadSafeQueue<int> _queue;
+        private readonly object _locker;
+
+        public Publisher(int id, ThreadSafeQueue<int> queue, object locker)
+        {
+            _id = id;
+            _queue = queue;
+            _locker = locker;
+        }
+
+        public void DoEnqueue()
+        {
+            try
+            {
+                lock (_locker)
+                {
+                    var num = RandomHelper.GetRandomNumber();
+                    _queue.Enqueue(num);
+                    Console.WriteLine($"=> PUB[{_id}]: Item enqueued: \"{num}\"!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+        }
+    }
+}
